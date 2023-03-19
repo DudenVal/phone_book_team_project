8e9b677bb72e08b505842e7442811e1b84bb8fb5
DudenVal <valerapro33@yandex.ru>
2023-03-19 13:10:28 +0300

    правки

--- read.py
+++ read.py
@@ -1,3 +1,21 @@
-def readfile(filename):
-    return open(filename).read().split('\n')
- 
+
+
+def read():
+    global last_id, all_data
+    with open(file_base, "r",encoding="utf-8") as f:
+        all_data = [i.strip() for i in f]
+
+
+
+# -------------------------правки-------------------
+def read():
+
+    global all_data, last_id
+
+    with open(filename, "r", encoding="utf-8") as f:
+        all_data = [i.strip() for i in f]
+        if all_data:
+            last_id = int(all_data[-1].split()[0])
+
+            return all_data
+    
